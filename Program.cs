7f6306a9f9014cb33e7330bf484177c7cdc82396
Rejwan Raouf <rejwanraouf@Rejwans-MacBook-Pro.local>
2024-09-16 19:19:30 +0200

    Implemented addition feature

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Velkommen til Kalkulatorapp!");
+
+// Addisjon 
+Console.WriteLine("Skriv inn første tall for addisjon");
+double tall1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Skriv inn andre tall for addisjon");
+double tall2 = Convert.ToDouble(Console.ReadLine());
+
+double sum = tall1 + tall2;
+
+Console.WriteLine($"Resultat av addisjon: {sum}");
