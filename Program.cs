ca4dbd76d2f3e3df3ee3d5357081f3d6bcd55fdd
Stevan M. Vågen <stevanvaagen@gmail.com>
2024-09-16 19:32:51 +0200

    added function to calculate Rejwans age

--- Program.cs
+++ Program.cs
@@ -10,3 +10,9 @@ double tall2 = Convert.ToDouble(Console.ReadLine());
 double sum = tall1 + tall2;
 
 Console.WriteLine($"Resultat av addisjon: {sum}");
+
+
+
+// finn Rejwans alder
+int year = DateTime.Now.Year;
+Console.WriteLine($"Rejwan's alder er {year - 1997}");
